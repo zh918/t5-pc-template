cccd3adb3b2acc3a03244f02722e74425f7c3762
zh918 <287360367@qq.com>
2020-06-23 18:03:31 +0800

    .

--- build/webpack.build.config.js
+++ build/webpack.build.config.js
@@ -5,5 +5,10 @@ const merge = require('webpack-merge')
 const baseWebpackConfig = require('./webpack.base.config')
 
 module.exports = merge(baseWebpackConfig, {
-  plugins: [ new CleanWebpackPlugin() ]
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.DefinePlugin({
+      'process.env': {NODE_ENV: `"${process.env.NODE_ENV}"`} 
+    })
+  ]
 });
